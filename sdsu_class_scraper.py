ca95df0130960923047bd2a468389d3092513bd3
jaxonhill <jaxoncharleshill@gmail.com>
2023-02-01 21:36:41 -0800

    Add TODOs

--- sdsu_class_scraper.py
+++ sdsu_class_scraper.py
@@ -11,10 +11,11 @@ from classes.StartInstance import StartInstance
 def main():
     # Create the start driver and select a term based on user choice
     start_driver: StartInstance = StartInstance()
+    # TODO: Error checking the term choice based on the options??
     term_choice: str = input(f"{start_driver.getTermOptions()}\n")
     start_driver.clickChosenTerm(term_choice)
 
-    #
+    # TODO: Get all possible categories and store in an array
 
 
 if __name__ == "__main__":
